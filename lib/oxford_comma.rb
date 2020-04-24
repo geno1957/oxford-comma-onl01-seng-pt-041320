5ae54bc04ad537690f4fe7d36e556cefa8da9b5a
Jared  E <jared.epps322@gmail.com>
2020-04-24 03:29:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,8 @@ def oxford_comma(array)
   if array.length == 1
     array[0]
   elsif array.length == 2 
-    array[1]   array.join("and")
-    
+    array[1] 
+    array.join("and")
   else 
     array[array.length-1 ] = "and " << array[array.length-1 ]
     array.join(", ") 
